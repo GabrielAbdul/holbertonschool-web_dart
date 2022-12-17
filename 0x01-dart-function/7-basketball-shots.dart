86d0c61d526e5e1d4f3610bdb5c16aa98f77b0d1
Gabriel Abdul <Gabrielabdul1@gmail.com>
2022-12-16 22:01:51 -0600

    calculates the total number of points for each team and return the winning team:

---
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -0,0 +1,27 @@
+int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+    int teamAScore = 0;
+    int teamBScore = 0;
+
+    teamAScore = calculateTeamScore(teamA);
+    teamBScore = calculateTeamScore(teamB);
+
+    if (teamAScore > teamBScore) return 1;
+    if (teamAScore < teamBScore) return 2;
+
+    return 0;
+}
+
+int calculateTeamScore(Map<String, int> team) {
+    int? freeThrows = 0;
+    int twoPointers = 0;
+    int threePointers = 0;
+    int teamScore = 0;
+
+    freeThrows = (team['Free throws'] ?? 0);
+    twoPointers = (team['2 pointer'] ?? 0) * 2;
+    threePointers = (team['3 pointer'] ?? 0) * 3;
+
+    teamScore = freeThrows + twoPointers + threePointers as int;
+
+    return teamScore;
+}
